10ef1191f307f376925d7555528b183406f7672a
Conrado Quilles Gomes <conradoqg@gmail.com>
2017-03-25 09:41:15 -0300

    Moves p5 extensions to its own file and adds a func to calc coors from a rect (angled or not).

--- index.js
+++ index.js
@@ -8,18 +8,6 @@
 require('./plugins/*.js', { mode: 'expand' });
 
 p5.disableFriendlyErrors = true;
-p5.Vector.prototype.rotateOnOrigin = function (origin, angle) {
-    if (this.p5) {
-        if (this.p5._angleMode === this.p5.DEGREES) {
-            angle = this.p5.polarGeometry.degreesToRadians(angle);
-        }
-    }
-    var newX = Math.cos(angle) * (this.x - origin.x) - Math.sin(angle) * (this.y - origin.y) + origin.x;
-    var newY = Math.sin(angle) * (this.x - origin.x) + Math.cos(angle) * (this.y - origin.y) + origin.y;
-    this.x = newX;
-    this.y = newY;
-    return this;
-};
 
 const World = require('./world.js');
 const word = new World();
